dce142fe80de3cdb33521a7ca4bb1324ad8dcd07
Bruno Gonçalves <brunogon463@gmail.com>
2024-08-28 18:20:08 -0300

    att

---
+++ Lista de Exercicios - 02/Exercicio - 06.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+      
+      int impar, par, valor;
+      
+      printf("Digite um numero:" );
+            scanf("%d", &valor);
+      
+      if (valor % 2 == 0) {
+            printf("O Numero é par!");          
+      } else {
+            printf("O Numero é impar!");
+      }
+      
+}
--- Lista de Exercicios - 02/Exercicio - 08.cpp
+++ Lista de Exercicios - 02/Exercicio - 08.cpp
@@ -3,16 +3,37 @@
 #include <math.h>
 
 int main() {
-      
-      int impar, par, valor;
-      
-      printf("Digite um numero:" );
-            scanf("%d", &valor);
-      
-      if (valor % 2 == 0) {
-            printf("O Numero é par!");          
-      } else {
-            printf("O Numero é impar!");
-      }
-      
+	
+	float n1, n2, opcao, somar, raiz;
+		
+	printf("1 - Somar dois números: \n");
+	printf("2 - Raiz quadrada de um numero: \n");
+	printf("Digite a opção desejada: ");
+		scanf("%f", &opcao);
+		
+	if (opcao > 3 ) {
+		printf("ERRO! DIGITE UMA OPÇÃO VALIDA!");
+	} else if (opcao == 1) {
+		system("cls");
+
+		printf("Digite o primeiro número: ");
+			scanf("%f", &n1);
+		
+		printf("Digite o segundo número: ");
+			scanf("%f", &n2);
+	
+		somar = n1 + n2;
+	
+		printf("A soma dos dois numeros e de: %.2f", somar);
+		
+	} else if (opcao == 2) {
+		system("cls");
+
+		printf("Digite um numero: ");
+			scanf("%f", &n1);
+		
+		raiz =sqrt(n1);
+		
+		printf("A raiz de %.2f e: %.2f ", n1, raiz);
+	}
 }
